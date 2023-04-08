52af760876a9e82b1a8c8c372f7c2ced23a638bb
Denys07748 <denis07748@gmail.com>
2023-04-08 13:44:13 +0300

    рефакторінг форми з використанням Formik & Yup

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -55,37 +55,66 @@
 //         )
 //     }
 // }
+import PropTypes from 'prop-types';
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as yup from "yup";
+
+const { validName: { checkName, messageName }, validNum: { checkNum, messageNum } } = {
+    validName: {
+        checkName: /^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$/,
+        messageName: 'Name may contain only letters, apostrophe, dash and spaces',
+    },
+    validNum: {
+        checkNum: /\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}/,
+        messageNum: 'Phone number must be digits and can contain spaces, dashes, parentheses and can start with +',
+    },
+}
+
+const schema = yup.object().shape({
+    name: yup.string().matches(checkName, messageName).required('Name is required'),
+    number: yup.string().matches(checkNum, messageNum).required('Number is required'),
+});
+
+const initialValues = {
+    name: '', 
+    number: '',
+};
 
 const ContactForm = ({onSubmit}) => {
-    const handleSubmit = e => {
-        e.preventDefault();
+    const handleSubmit = (values, {resetForm}) => {
+        console.log(values);
 
-        const { name, number } = e.target.elements;
-        console.log({name: name.value, number: number.value});
-        const contact = {
-            name: name.value, 
-            number: number.value,
-        };
-        onSubmit(contact);
-    }
+        onSubmit(values);
+        resetForm();
+    };
 
         return (
-            <form onSubmit={handleSubmit}>
-                <label htmlFor='name'>
-                    Name<input
-                        type="text"
-                        name="name"
-                    />
-                </label>
-                <label htmlFor='number'>
-                    Number<input
-                        type="tel"
-                        name="number"
-                    />
-                </label>
-                <button type='submit'>Add contact</button>
-            </form>
+            <Formik initialValues={initialValues} 
+                    validationSchema={schema} 
+                    onSubmit={handleSubmit}>
+                <Form >
+                    <label htmlFor='name'>
+                        Name<Field
+                            type="text"
+                            name="name"
+                        />
+                        <ErrorMessage name="name" component='div'/>
+                    </label>
+                    <label htmlFor='number'>
+                        Number<Field
+                            type="tel"
+                            name="number"
+                        />
+                        <ErrorMessage name="number" component='div'/>
+                    </label>
+                    <button type='submit'>Add contact</button>
+                </Form>
+            </Formik>
         );
 }
 
 export default ContactForm;
+
+ContactForm.propTypes = {
+    onSubmit: PropTypes.func.isRequired,
+};
